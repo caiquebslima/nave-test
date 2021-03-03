5a40d668d6732e9db0790fdf511a65f4bade9126
caiquebslima <caiquebslima@gmail.com>
2021-03-03 19:29:52 -0300

    feat: added login page structure and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import './App.scss';
+import LoginPage from './pages/LoginPage';
+import './styles/app.scss';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <LoginPage />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,12 +1,46 @@
 import React from 'react';
+import { TextField, Button } from '@material-ui/core/';
+
+import NaveLogo from '../assets/NaveLogo';
+import '../styles/login-page.scss';
 
 // TODO: apply correct type to function component
 const LoginPage: React.FC<any> = () => {
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log('form submitted!');
+  };
+
   return (
-    <main>
-      <div>
-        <h1>login</h1>
-      </div>
+    <main className='container login'>
+      <section className='login__box'>
+        <figure>
+          <NaveLogo />
+        </figure>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            id='email'
+            label='E-mail'
+            variant='outlined'
+            className='input-spacing'
+            fullWidth={true}
+          />
+          <TextField
+            id='password'
+            label='Senha'
+            variant='outlined'
+            className='input-spacing'
+            fullWidth={true}
+          />
+          <Button
+            type='submit'
+            className='input-spacing nave-button'
+            fullWidth={true}
+          >
+            Entrar
+          </Button>
+        </form>
+      </section>
     </main>
   );
 };
