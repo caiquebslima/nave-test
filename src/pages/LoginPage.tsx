ba3bef883ede6766da7d85c3de4415dfec1d202c
caiquebslima <caiquebslima@gmail.com>
2021-03-03 18:35:28 -0300

    feat: added logo icon feat: created basic login page structure feat: created basic apicontext structure feat: added react-router-dom types

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// TODO: apply correct type to function component
+const LoginPage: React.FC<any> = () => {
+  return (
+    <main>
+      <div>
+        <h1>login</h1>
+      </div>
+    </main>
+  );
+};
+
+export default LoginPage;
