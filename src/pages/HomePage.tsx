1e853335c93a99ae35f9ec28dd28d4ec67f2186a
caiquebslima <caiquebslima@gmail.com>
2021-03-03 22:32:03 -0300

    feat: added sample data for test feat: created card component for home feat: created action bar on home

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,30 @@
+import { Button } from '@material-ui/core';
 import React from 'react';
+
 import Header from '../components/Header';
+import Card from '../components/Card';
+import { sampleData } from '../components/sampleData';
+
+import '../styles/home-page.scss';
 
 const HomePage: React.FC<any> = () => {
   return (
     <React.Fragment>
       <Header />
-      <main>
-        <h1>Homepage</h1>
+      <main className='people'>
+        <section className='people__action-bar'>
+          <h2>Navers</h2>
+          <Button variant='contained' className='nave-button'>
+            Adicionar Naver
+          </Button>
+        </section>
+        <section className='people__cards row'>
+          {sampleData.map((person) => {
+            return (
+              <Card name={person.name} job={person.job} photo={person.photo} />
+            );
+          })}
+        </section>
       </main>
     </React.Fragment>
   );
